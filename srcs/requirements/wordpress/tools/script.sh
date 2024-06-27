f0e6da38947e25826984708cb8e8db659424f521
RABAT Martin <martin.Rabat@gmail.Com>
2024-06-27 15:42:13 +0200

    add htts to worpress init

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -16,7 +16,8 @@ done
 ./wp-cli.phar config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 
 # Instation de wordpress
-./wp-cli.phar core install --url='https://${DOMAIN_NAME}' --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+urlwp="https://${DOMAIN_NAME}"
+./wp-cli.phar core install --url=$urlwp --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 ./wp-cli.phar user create $WORDPRESS_USER $WORDPRESS_USER_MAIL --role=author --user_pass=$WORDPRESS_PASSWORD --allow-root
 
 php-fpm8.2 -F
